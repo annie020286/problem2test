5f7e4fa98ff09b27fb053ffbfd3df2b7b17e4094
annie020286 <91325602+annie020286@users.noreply.github.com>
2021-09-24 23:08:08 +0800

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -4,36 +4,38 @@ using namespace std ;
 
 int main()
 {
-   int n = 0 ;
-   int m = 0 ;
-   cin >> n >> m ; //read two integers from PDOGS
+    int n = 0 ;
+    int m = 0 ;
+    cin >> n >> m ;    //read two integers from PDOGS
    
-   if(n < m ) // To check if n > m 
-     {
-         
-     cout << -1  ;
-     
-     }
+    if(n < m){        // To check if n > m 
+       cout << -1  ;
+    }
+    
+    if(n >= m){           // To check if n > m     
+
+        int leader = 1 ;               
+// The leading student number of first group
+
+        int taNum= n / m ;       
+ // The number of students that goes to the teacher's team
+
+        int num = taNum * m ;
+        
+        while(leader <= num){
+            cout << leader ;        
+            if(leader + m < num){       // To check if 'leadNum' is the last one leadNum
+                cout << ','  ; 
+            }
+            leader = leader + m  ; 
+// add the number of each groups to the leadNum
+        }
+    }
+
    
-  if(n >= m)  // To check if n > m 
-     {
-         
-      int leadNum = 1 ; // The leading student number of first group
-      
-      int teacherNum = n % m ;  // The number of students that goes to the teacher's team
-      
-      int taNum =n - teacherNum ; // The number of students that goes to the TAs
-      
-      while(leadNum <= taNum ) 
-        {
-            
-         cout<< leadNum ;
-         
-          if (leadNum + m < taNum) // To check if 'leadNum' is the last one leadNum
-          
-           {
-               cout << ','  ; 
-           }
+    return 0 ;  // indicate that the program ended successfully
+    
+}
          
          leadNum= leadNum + m  ; // add the number of each groups to the leadNum
 
